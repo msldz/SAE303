7d1bc092d3d37597ddea7542d68215dc2712a0d5
Massil <massil.benna17@gmail.com>
2026-01-13 18:12:14 +0100

    ajout de la pop up

--- fetch.js
+++ fetch.js
@@ -1,24 +1,39 @@
-// 1. Initialisation de la carte
+// 1. Mise en place de la carte
 // On cible la div avec l'id 'map'
-// setView([latitude, longitude], zoom) -> On centre sur Paris (IDF)
+// setView([latitude, longitude], zoom) veut dire qu'on se centre sur Paris (IDF)
 const maCarte = L.map('map').setView([48.8566, 2.3522], 11);
 
-// 2. Ajout des "tuiles" (le visuel de la carte)
-// On utilise OpenStreetMap qui est gratuit
+// 2. Ajout du visuel de la carte
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(maCarte);
 
 
-// --- Ton code précédent reste ici ---
-
 async function afficherBoulangeriesIDF() {
      const reponse = await fetch("geo-boulangeries-ble-idf.json");
      const boulangeries = await reponse.json();
 
-     console.log(boulangeries);
-     // C'est ici qu'on ajoutera les marqueurs plus tard
+     // boucle pour que sur chaque boulangerie on a le marqueur de créer et la pop up avec le nom de la boulangerie, l'adresse, le code postale et la ville
+     boulangeries.forEach(boulangerie => {
+         
+         // marqueur
+         let monMarqueur = L.marker([boulangerie.latitude, boulangerie.longitude]);
+
+         //pop up
+         monMarqueur.bindPopup(`
+             <div class="popup-contenu">
+                 <h3 class="popup-titre">${boulangerie.nom}</h3>
+                 <p class="popup-adresse">
+                    ${boulangerie.adresse}<br>
+                    <strong>${boulangerie.cp} ${boulangerie.ville}</strong>
+                 </p>
+             </div>
+         `);
+
+         //  ajout du marqueur à la carte
+         monMarqueur.addTo(maCarte);
+     });
 }
 
 afficherBoulangeriesIDF();
